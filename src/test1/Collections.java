caa480b213fa53988159b81f1bbfb8775277f54c
Nordine59 <itounour@hotmail.fr>
2016-09-15 17:01:29 +0200

    Premiere Version modif

---
+++ src/test1/Collections.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author admin
+ */
+public class Collections {
+    
+    
+    public static void main(String[] args) {
+        
+     
+            
+            
+        
+        
+    
+       
+        
+        
+       ArrayList<Integer> liste1 = new ArrayList<Integer>();
+          int l1 = 0 + 10;
+       
+    for (i = 2,i <= 99){
+        
+        
+        
+        
+    
+        
+         
+         
+     }
+    
+    
+       
+//        
+//        liste.add("coucou");
+//        liste.add("tout le monde");
+//        liste.remove("coucou");
+//        
+//        liste.remove(2);
+//        
+//        
+//        
+//     
+//        
+//        System.out.println(liste + " " + liste.size());
+//        
+//        System.out.println(liste.indexOf("coucou"));
+//        
+//        ArrayList ListeAsupprimer = new ArrayList();
+//        
+//        
+//        
+//        
+//        
+//        
+//        liste.removeAll(liste);
+//        
+//        System.out.println(liste.get(2));
+//        
+
+
+      
+          
+          
+
+
+
+
+
+
+
+    }
+        
+        
+        
+       
+        
+    }
+
